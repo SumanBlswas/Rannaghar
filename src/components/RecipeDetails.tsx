d9ed1e55fa2ef2794bf475e994d8c9be9d8d6bfc
Suman Biswas <sumanmanner@gmail.com>
2023-09-18 23:49:03 +0530

    added more thing in Recipedetails

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -11,6 +11,8 @@ import {
   Divider,
   List,
   ListItem,
+  Link,
+  SimpleGrid,
 } from "@chakra-ui/react";
 import { getParamsRecipe } from "../api/recipeApi";
 
@@ -20,12 +22,54 @@ interface Recipe {
   title: string;
   image: string;
   summary: string;
-  extendedIngredients: { id: number; originalString: string }[];
-  analyzedInstructions: { steps: { number: number; step: string }[] }[];
+  extendedIngredients: {
+    id: number;
+    originalString: string;
+    name: string; // Add 'name' property
+    image: string; // Add 'image' property
+  }[];
+  analyzedInstructions: {
+    name: string;
+    steps: {
+      number: number;
+      step: string;
+      ingredients: {
+        id: number;
+        name: string;
+        localizedName: string;
+        image: string;
+      }[];
+      equipment: {
+        id: number;
+        name: string;
+        localizedName: string;
+        image: string;
+      }[];
+      length?: {
+        number: number;
+        unit: string;
+      };
+    }[];
+  }[];
   cuisines: string[];
   servings: number;
   readyInMinutes: number;
   diets: string[];
+  winePairing: {
+    pairedWines: string[];
+    pairingText: string;
+    productMatches: {
+      id: number;
+      title: string;
+      description: string;
+      price: string;
+      imageUrl: string;
+      averageRating: number;
+      ratingCount: number;
+      score: number;
+      link: string;
+    }[];
+  };
 }
 
 const RecipeDetails = () => {
@@ -37,8 +81,6 @@ const RecipeDetails = () => {
     getParamsRecipe(id)
       .then((data: Recipe) => {
         setRecipe(data);
-        console.log(data);
-        
       })
       .catch((error) => {
         console.error("Error fetching recipe details:", error);
@@ -83,7 +125,7 @@ const RecipeDetails = () => {
 
       <Text fontSize="lg" mb={4}>
         {/* Render HTML content safely */}
-        <Box dangerouslySetInnerHTML={renderHTML(recipe.summary)} />
+        <div dangerouslySetInnerHTML={renderHTML(recipe.summary)} />
       </Text>
 
       <Divider mb={4} />
@@ -91,22 +133,100 @@ const RecipeDetails = () => {
       <Heading as="h2" size="lg" mb={2}>
         Ingredients:
       </Heading>
-      <List spacing={1}>
+      <SimpleGrid columns={2} spacing={4}>
         {recipe.extendedIngredients.map((ingredient) => (
-          <ListItem key={ingredient.id}>{ingredient.originalString}</ListItem>
+          <Box key={ingredient.id}>
+            <Image
+              src={`https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}`}
+              alt={ingredient.name}
+              maxH="100px"
+            />
+            <Text fontSize="lg">{ingredient.name}</Text>
+          </Box>
         ))}
-      </List>
+      </SimpleGrid>
 
       <Divider mt={4} mb={4} />
 
       <Heading as="h2" size="lg" mb={2}>
         Instructions:
       </Heading>
-      <ol>
-        {recipe.analyzedInstructions[0]?.steps.map((step) => (
-          <li key={step.number}>{step.step}</li>
-        ))}
-      </ol>
+      {recipe.analyzedInstructions.map((instruction, index) => (
+        <Box key={index} mb={4}>
+          <Text fontSize="lg" mb={2}>
+            <strong>{instruction.name}</strong>
+          </Text>
+          <ol>
+            {instruction.steps.map((step) => (
+              <li key={step.number}>
+                {step.step}
+                {step.ingredients.length > 0 && (
+                  <div>
+                    <strong>Ingredients:</strong>{" "}
+                    {step.ingredients
+                      .map((ingredient) => ingredient.localizedName)
+                      .join(", ")}
+                  </div>
+                )}
+                {step.equipment.length > 0 && (
+                  <div>
+                    <strong>Equipment:</strong>{" "}
+                    {step.equipment
+                      .map((equipment) => equipment.localizedName)
+                      .join(", ")}
+                  </div>
+                )}
+                {step.length && (
+                  <div>
+                    <strong>Cooking Time:</strong> {step.length.number}{" "}
+                    {step.length.unit}
+                  </div>
+                )}
+              </li>
+            ))}
+          </ol>
+        </Box>
+      ))}
+
+      <Divider mt={4} mb={4} />
+
+      <Heading as="h2" size="lg" mb={2}>
+        Wine Pairing:
+      </Heading>
+      <VStack align="start" spacing={4}>
+        <Text fontSize="lg">
+          <strong>Paired Wines:</strong>{" "}
+          {recipe.winePairing.pairedWines.join(", ")}
+        </Text>
+        <Text fontSize="lg">
+          <strong>Pairing Text:</strong> {recipe.winePairing.pairingText}
+        </Text>
+        <Text fontSize="lg">
+          <strong>Recommended Wine:</strong>{" "}
+          {recipe.winePairing.productMatches[0].title}
+        </Text>
+        <Image
+          src={recipe.winePairing.productMatches[0].imageUrl}
+          alt="Wine"
+          maxH="200px"
+        />
+        <Text fontSize="lg">
+          <strong>Price:</strong> {recipe.winePairing.productMatches[0].price}
+        </Text>
+        <Text fontSize="lg">
+          <strong>Rating:</strong>{" "}
+          {recipe.winePairing.productMatches[0].averageRating} (Based on{" "}
+          {recipe.winePairing.productMatches[0].ratingCount} ratings)
+        </Text>
+        <Link
+          fontSize="lg"
+          href={recipe.winePairing.productMatches[0].link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Buy Wine
+        </Link>
+      </VStack>
     </Box>
   );
 };
